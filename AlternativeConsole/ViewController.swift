05cf4731567cffe52274d9d8a50a52c960f710b7
Danilo de Sousa <danspaulo@gmail.com>
2021-12-28 20:29:44 -0300

    Initial Commit

---
+++ AlternativeConsole/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AlternativeConsole
+//
+//  Created by Danilo de Sousa on 28/12/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
